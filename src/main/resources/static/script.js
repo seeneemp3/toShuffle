f9682218daf0060ed08d166e001eacf58e71aab1
seeneemp3 <seeneemp3@gmail.com>
2023-11-10 18:37:32 +0400

    three pages is ok

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,9 +1,7 @@
 function toSearch() {
     document.getElementById("welcomePage").style.display = "none";
     document.getElementById("searchPage").style.display = "block";
-    document.getElementById("showPlaylistPage").style.display = "none";
   }
-// вынести название плейлиста и кнопки назад сделать
 
   function toFinish() {
     document.getElementById("showPlaylistPage").style.display = "none";
@@ -16,7 +14,26 @@ function toSearch() {
   }
 
   $(document).ready(function() {
-
+    function search(input, resultsContainer) {
+        const query = $(input).val();
+      
+        // Mock data array with three items
+        const mockData = ['Artist 1', 'Artist 2', 'Artist 3'];
+      
+        // Generating the results HTML with three mocked items
+        let resultsHtml = '<ul>';
+      
+        mockData.forEach(artist => {
+          resultsHtml += '<li class="search-result-item">' + artist + '</li>';
+        });
+      
+        resultsHtml += '</ul>';
+      
+        // Set the inner HTML of the results container
+        $(resultsContainer).html(resultsHtml).show();
+      }
+      
+/*
       function search(input, resultsContainer) {
           const query = $(input).val();
 
@@ -32,6 +49,7 @@ function toSearch() {
               $(resultsContainer).html(resultsHtml).show();
           });
       }
+      */
 
       function setupInput(inputSelector, resultsSelector) {
           var timeout = null;
@@ -52,6 +70,12 @@ function toSearch() {
               $(inputSelector).val(selectedItemText);
               $(resultsSelector).hide();
           });
+          
+          $(document).on('click', function(e) {
+            if (!$(e.target).closest(resultsSelector).length && !$(e.target).is(inputSelector)) {
+              $(resultsSelector).hide(); 
+            }
+          });
       }
 
       setupInput('#search-input1', '#search-results1');
@@ -59,48 +83,117 @@ function toSearch() {
       setupInput('#search-input3', '#search-results3');
   });
 
+ 
+
+
   document.addEventListener('DOMContentLoaded', function() {
-      var form = document.getElementById('sendInputs');
-      var submitButton = document.getElementById('submit-button');
+    var form = document.getElementById('sendInputs');
+    var submitButton = document.getElementById('submit-button');
 
-      submitButton.addEventListener('click', function(event) {
-          document.getElementById("searchPage").style.display = "none";
-          document.getElementById("showPlaylistPage").style.display = "block";
-          event.preventDefault();
+    submitButton.addEventListener('click', function(event) {
+        document.getElementById("searchPage").style.display = "none";
+        document.getElementById("showPlaylistPage").style.display = "flex";
+        event.preventDefault();
 
-          var queryParams = new URLSearchParams(new FormData(form)).toString();
-          var requestUrl = form.action + '?' + queryParams;
+        // Используйте мок данные
+        var data = getMockTracks();
+        parseAndDisplayResults(data);
+    });
 
-          fetch(requestUrl).then(response => {
-              if(response.ok) {
-                  return response.text();
-              }
-              throw new Error('Сетевая ошибка при запросе');
-          }).then(data => {
+    function getMockTracks() {
+        return JSON.stringify([
+            { name: "Трек 1", artists: [{ name: "Артист 1" }] },
+            { name: "Трек 2", artists: [{ name: "Артист 2" }] },
+            { name: "Трек 1", artists: [{ name: "Артист 1" }] },
+            { name: "Трек 2", artists: [{ name: "Артист 2" }] },
+            { name: "Трек 1", artists: [{ name: "Артист 1" }] },
+            { name: "Трек 2", artists: [{ name: "Артист 2" }] },
+            { name: "Трек 1", artists: [{ name: "Артист 1" }] },
+            { name: "Трек 2", artists: [{ name: "Артист 2" }] },
+            { name: "Трек 1", artists: [{ name: "Артист 1" }] },
+            { name: "Трек 1", artists: [{ name: "Артист 1" }] },
+            { name: "Трек 2", artists: [{ name: "Артист 2" }] },
+            { name: "Трек 1", artists: [{ name: "Артист 1" }] },
+            { name: "Трек 2", artists: [{ name: "Артист 2" }] },
+            { name: "Трек 1", artists: [{ name: "Артист 1" }] },
+            { name: "Трек 2", artists: [{ name: "Артист 2" }] }
 
-              console.log(data);
+        ]);
+    }
 
-              parseAndDisplayResults(data);
-          }).catch(error => {
-              console.error('Ошибка:', error);
-          });
-      });
+    function parseAndDisplayResults(data) {
+        var tracks = JSON.parse(data);
+        var resultsHtml = '<div class="scrollable-list">';
+      
+        tracks.forEach(function(track) {
+          resultsHtml += '<div class="track">' + 
+                         '<div class="track-info">' + 
+                         '<p class="track-name">' + track.name + '</p>' +
+                         '<p class="artist-name">' + track.artists.map(artist => artist.name).join(', ') + '</p>' +
+                         '</div>' +
+                         '</div>';
+        });
+        resultsHtml += '</div>';
 
-      function parseAndDisplayResults(data) {
-          var tracks = JSON.parse(data);
-          var resultsHtml = '<ul>';
+        var playlistPage = document.getElementById('showPlaylistPage');
+        var button = playlistPage.querySelector('button'); // Предполагаем, что кнопка - это элемент button
+        
+        // Вставка результатов перед кнопкой
+        if (button) {
+          button.insertAdjacentHTML('beforebegin', resultsHtml);
+        } else {
+          // Если кнопки нет, просто добавляем HTML в конец 'showPlaylistPage'
+          playlistPage.insertAdjacentHTML('beforeend', resultsHtml);
+        }
 
-          tracks.forEach(function(track) {
-              resultsHtml += '<li>' +
-                             '<strong>Трек:</strong> ' + track.name + '<br>' +
-                             '<strong>Артист:</strong> ' + track.artists.map(artist => artist.name).join(', ') +
-                             '</li>';
-          });
-          resultsHtml += '</ul>';
-          document.getElementById('showPlaylistPage').insertAdjacentHTML('beforeend', resultsHtml);
-
+        //document.getElementById('showPlaylistPage').insertAdjacentHTML('beforeend', resultsHtml);
       }
+      
+});
 
-  });
+
+//   document.addEventListener('DOMContentLoaded', function() {
+//       var form = document.getElementById('sendInputs');
+//       var submitButton = document.getElementById('submit-button');
+
+//       submitButton.addEventListener('click', function(event) {
+//           document.getElementById("searchPage").style.display = "none";
+//           document.getElementById("showPlaylistPage").style.display = "block";
+//           event.preventDefault();
+
+//           var queryParams = new URLSearchParams(new FormData(form)).toString();
+//           var requestUrl = form.action + '?' + queryParams;
+
+//           fetch(requestUrl).then(response => {
+//               if(response.ok) {
+//                   return response.text();
+//               }
+//               throw new Error('Сетевая ошибка при запросе');
+//           }).then(data => {
+
+//               console.log(data);
+
+//               parseAndDisplayResults(data);
+//           }).catch(error => {
+//               console.error('Ошибка:', error);
+//           });
+//       });
+
+//       function parseAndDisplayResults(data) {
+//           var tracks = JSON.parse(data);
+//           var resultsHtml = '<ul>';
+
+//           tracks.forEach(function(track) {
+//               resultsHtml += '<li>' +
+//                              '<strong>Трек:</strong> ' + track.name + '<br>' +
+//                              '<strong>Артист:</strong> ' + track.artists.map(artist => artist.name).join(', ') +
+//                              '</li>';
+//           });
+//           resultsHtml += '</ul>';
+//           document.getElementById('showPlaylistPage').insertAdjacentHTML('beforeend', resultsHtml);
+
+//       }
+
+//   });
 
 
